4c40d0d748ffa596c8f3a27da6530ccbcf472e01
Luís Henrique Castilho <luishecastilho@gmail.com>
2024-12-07 10:15:29 -0300

    Kick off

---
+++ App.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export default function App() {
+    return <div>
+        <h1>Heyooo</h1>
+        <p>It's me, your app</p>
+    </div>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "bundle.min.js",
+        assetModuleFilename: '[name][ext]',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    limit: 10000,
+                    name: 'assets/[name].[ext]',
+                },
+            },
+            {
+                test: /\.js/, // i could use /\.(js|jsx)$/ to include jsx files
+                exclude: /node_modules/,
+                options: {
+                    cacheDirectory: true,
+                    presets: [
+                        '@babel/preset-env',
+                        ['@babel/preset-react', { runtime: 'automatic' }],
+                    ]
+                },
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    }
+};
